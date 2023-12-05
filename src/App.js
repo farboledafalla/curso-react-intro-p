1ad0e0ced134445c08c1ac01eaa7c52d2727847b
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2023-12-05 00:32:25 -0500

    Se agrega comunicacion de componentes usando props

--- src/App.js
+++ src/App.js
@@ -9,6 +9,26 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
+// Todos por defecto
+const defaultTodos = [
+   {
+      text: 'Cortar cebolla',
+      completed: true,
+   },
+   {
+      text: 'Tomar el curso de Intro a React.js',
+      completed: false,
+   },
+   {
+      text: 'Llorar con la llorona',
+      completed: false,
+   },
+   {
+      text: 'LALALALALALA',
+      completed: true,
+   },
+];
+
 function App() {
    return (
       <React.Fragment>
@@ -16,9 +36,13 @@ function App() {
          <TodoSearch />
 
          <TodoList>
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {defaultTodos.map((todo) => (
+               <TodoItem
+                  key={todo.text}
+                  text={todo.text}
+                  completed={todo.completed}
+               />
+            ))}
          </TodoList>
 
          <CreateTodoButton />
