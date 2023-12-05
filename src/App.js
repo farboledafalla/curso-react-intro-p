d2d5b074f14e13ff640012816e6b8653490f3d67
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2023-12-04 23:41:48 -0500

    Se agregan los componentes de Todo Machine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import logo from './platzi.webp';
 import './App.css';
 
+// Componentes
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
 function App() {
    return (
       <div className='App'>
+         <TodoCounter />
+         <TodoSearch />
+
+         <TodoList>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+         </TodoList>
+
+         <CreateTodoButton />
+
          <header className='App-header'>
             <img src={logo} className='App-logo' alt='logo' />
             <p>
