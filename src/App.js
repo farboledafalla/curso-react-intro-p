c6575f66544bc17fb28f7532c16149147be7e2df
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-03-05 21:03:13 -0500

    Se retira codigo del archivo original

--- src/App.js
+++ src/App.js
@@ -22,20 +22,6 @@ function App() {
 
          <CreateTodoButton />
 
-         <header className='App-header'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <p>
-               Edita el archivo <code>src/App.js</code> y guarda para recargar.
-            </p>
-            <a
-               className='App-link'
-               href='https://platzi.com/reactjs'
-               target='_blank'
-               rel='noopener noreferrer'
-            >
-               Aprendamos React
-            </a>
-         </header>
       </div>
    );
 }
