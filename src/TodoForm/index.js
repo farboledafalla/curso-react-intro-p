ebde9708e8eb1f0948639a570b75e5b280993038
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-04-08 21:26:10 -0500

    Se agrega CreateTodoButton

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TodoContext } from '../TodoContext';
 import './TodoForm.css';
 
 function TodoForm() {
+   const { addTodo, setOpenModal } = React.useContext(TodoContext);
+
+   // Estado para guardar nombre del nuevo TODO
+   const [newTodoValue, setNewTodoValue] = useState('');
+
+   // Función para manejar la lógica del formulario
+   const onSubmit = (event) => {
+      // Evitat que se recargue la página al darle click en los botones
+      event.preventDefault();
+      // Enviar el nuevo TODO
+      addTodo(newTodoValue);
+      // Cerrar el formulario
+      setOpenModal(false);
+   };
+
+   const onCancel = (event) => {
+      setOpenModal(false);
+   };
+
+   const onChange = (event) => {
+      setNewTodoValue(event.target.value);
+   };
+
    return (
-      <form
-         onSubmit={(event) => {
-            // Evitat que se recargue la página al darle click en los botones
-            event.preventDefault();
-         }}
-      >
+      <form onSubmit={onSubmit}>
          <label>Escribe tu nuevo TODO</label>
-         <textarea placeholder='Cortar cebolla para el almuerzo' />
+         <textarea
+            placeholder='Cortar cebolla para el almuerzo'
+            value={newTodoValue}
+            onChange={onChange}
+         />
          <div className='TodoForm-buttonContainer'>
             <button
                type='button'
                className='TodoForm-button TodoForm-button--cancel'
+               onClick={onCancel}
             >
                Cancelar
             </button>
